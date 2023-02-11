acbce4e0b0d8ccf1622c2be378883582666b4960
owenfeehan <owenfeehan@users.noreply.github.com>
2023-02-11 11:11:55 +0000

    Deploying to gh-pages from  @ 242668e789cdba8742d11e2de2e40d55920153d2 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev1+g2a3eea9',
+    VERSION: '0.0.post1.dev1+g242668e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
